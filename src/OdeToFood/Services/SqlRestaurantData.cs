2946c0151fd9242da15c724d4186be94f48f7bbb
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2018-09-26 20:16:46 +0530

    implemented DbContext

---
+++ src/OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext odeToFoodDbContext)
+        {
+            _context = odeToFoodDbContext;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+            _context.SaveChanges();
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
