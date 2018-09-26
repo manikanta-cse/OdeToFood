5b31fd86a6e2add6821cdfb70a5bfe4d4cc68fe1
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2018-09-26 12:21:20 +0530

    attribute routing

--- src/OdeToFood/Controllers/AboutController.cs
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    //[Route("about")]
+    [Route("company/[controller]/[action]")] // token based, more robust
     public class AboutController
     {
+        //[Route("")]
         public string Phone()
         {
             return "+91-66564564546546";
         }
 
+        //[Route("address")]
+        //[Route("[action]")]
         public string Address()
         {
             return "India";
