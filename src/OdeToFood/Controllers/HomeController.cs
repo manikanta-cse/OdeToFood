e30a589bd528312bc0ec2b193da88d2dfca1f31a
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2018-09-26 17:48:02 +0530

    added service to return data and render in view

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,20 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
         public IActionResult Index()
         {
             //return Content("Hello from the HomeController");
 
-            var model = new Restaurant { Id = 1, Name = " My Pizza place" };
+            // var model = new Restaurant { Id = 1, Name = " My Pizza place" };
+
+            var model = _restaurantData.GetAll();
 
             //return new ObjectResult(model);
 
---
+++ src/OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+
+        public InMemoryRestaurantData()
+        {
+
+
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id=1,Name="My Pizza Place"},
+                new Restaurant{Id=2,Name="Over the moon"},
+                new Restaurant{Id=3,Name="Foodie"}
+
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(o => o.Name);
+        }
+
+
+        List<Restaurant> _restaurants;
+    }
+}
